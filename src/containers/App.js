67565d98da30333b6cd4767f088bc8fb12fd5212
bbtora <23010526+bbtora@users.noreply.github.com>
2018-10-07 20:47:52 +0530

    async function

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,10 +13,10 @@ class App extends Component {
         }
     }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users}));        
+    async componentDidMount() {
+        const data = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await data.json();
+        this.setState({ robots: users });        
     }
 
     onSearchChange = (event) => {
