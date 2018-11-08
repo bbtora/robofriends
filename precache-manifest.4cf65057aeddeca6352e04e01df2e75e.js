16a81ce6d62adc230bb11307167dea6b0283572a
bbtora <23010526+bbtora@users.noreply.github.com>
2018-11-08 12:50:11 +0530

    Updates

---
+++ precache-manifest.4cf65057aeddeca6352e04e01df2e75e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "0d0009a0358ff21fc93b",
+    "url": "/robofriends/static/js/main.0d0009a0.chunk.js"
+  },
+  {
+    "revision": "9eb37de0af66caedebad",
+    "url": "/robofriends/static/js/1.9eb37de0.chunk.js"
+  },
+  {
+    "revision": "0d0009a0358ff21fc93b",
+    "url": "/robofriends/static/css/main.7e693c2e.chunk.css"
+  },
+  {
+    "revision": "9eb37de0af66caedebad",
+    "url": "/robofriends/static/css/1.5dfe1a89.chunk.css"
+  },
+  {
+    "revision": "d957e8ab9df34bb1fc137b08ba459c84",
+    "url": "/robofriends/index.html"
+  }
+];
